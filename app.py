abd8069233a13ffb0ed442d36cbcf6d5bfa941fd
csuzs <cszsombi95@gmail.com>
2024-08-19 16:48:29 +0200

    synonym update added on UI

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import io
 import logging
 import os
 import threading
-from typing import List
+from collections import defaultdict
+from typing import Dict, List
 
 import pandas as pd
 from fastapi import (BackgroundTasks, FastAPI, File, HTTPException, Request,
@@ -36,14 +37,17 @@ settings = Settings()
 app = FastAPI()
 logger = logging.getLogger("fastapi")
 # In-memory storage for tags
-app.names_storage: List[str] = []
+app.names_storage = []
+app.tag_synonyms = defaultdict(set)
 
 # Placeholder for the semantic search components
 embedder = Embedder(model_name=settings.model_name, cache_dir=settings.cache_dir)
-vec_db = VecDB(host=settings.qdrant_host,
-               port=settings.qdrant_port,
-               collection=settings.qdrant_collection,
-               vector_size=embedder.embedding_dim)
+vec_db = VecDB(
+    host=settings.qdrant_host,
+    port=settings.qdrant_port,
+    collection=settings.qdrant_collection,
+    vector_size=embedder.embedding_dim,
+)
 app.fuzzy_matcher = FuzzyMatcher([])
 
 # Flag to track background task status
@@ -66,26 +70,35 @@ async def log_requests(request: Request, call_next):
 @app.post("/upload-csv/")
 async def upload_csv(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     if task_running.is_set():
-        raise HTTPException(status_code=400, detail="A task is already running. Please try again later.")
+        raise HTTPException(
+            status_code=400, detail="A task is already running. Please try again later."
+        )
 
-    if not file.filename.endswith('.csv'):
-        raise HTTPException(status_code=400,
-                            detail="Invalid file format. Please upload a CSV file (needs to env with '.csv'.")
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid file format. Please upload a CSV file (needs to env with '.csv'.",
+        )
 
     content = await file.read()
     df = pd.read_csv(io.BytesIO(content), sep=None, header=0)
 
-    if 'name' not in df.columns:
-        raise HTTPException(status_code=400, detail="CSV file must contain a 'name' column.")
+    if "name" not in df.columns:
+        raise HTTPException(
+            status_code=400, detail="CSV file must contain a 'name' column."
+        )
 
-    names_storage = df['name'].dropna().unique().tolist()
+    names_storage = df["name"].dropna().unique().tolist()
 
     if len(names_storage) == 0:
         raise HTTPException(status_code=400, detail="No names found in the CSV file.")
 
     # Return error if the collection is already existing (and it's populated)
     if vec_db.collection_exists():
-        raise HTTPException(status_code=400, detail="Collection already exists. Please delete the collection first.")
+        raise HTTPException(
+            status_code=400,
+            detail="Collection already exists. Please delete the collection first.",
+        )
     else:
         vec_db._create_collection()
 
@@ -95,7 +108,9 @@ async def upload_csv(background_tasks: BackgroundTasks, file: UploadFile = File(
     # Add background task to process the CSV file
     background_tasks.add_task(process_csv, names_storage)
 
-    return {"message": "File accepted for processing. Names will be extracted and stored in the background."}
+    return {
+        "message": "File accepted for processing. Names will be extracted and stored in the background."
+    }
 
 
 def process_csv(names_storage: List[str]):
@@ -132,8 +147,13 @@ async def search(query: str, k: int = 5):
     semantic_matches = vec_db.find_closest(query_vector, k)
 
     # Formatting the response
-    semantic_results = [{"name": match.payload["name"], "score": match.score} for match in semantic_matches]
-    typo_results = [{"name": match["matched"], "score": match["score"]} for match in fuzzy_matches]
+    semantic_results = [
+        {"name": match.payload["name"], "score": match.score}
+        for match in semantic_matches
+    ]
+    typo_results = [
+        {"name": match["matched"], "score": match["score"]} for match in fuzzy_matches
+    ]
 
     response = {"match": {"semantic": semantic_results, "typo": typo_results}}
 
@@ -148,6 +168,21 @@ async def task_status():
         return {"status": "finished", "nb_items_stored": vec_db.get_item_count()}
 
 
+@app.post("/add_synonym/")
+async def update_synonym(tag_1: str, tag_2: str):
+
+    if tag_1 != tag_2:
+        app.tag_synonyms[tag_1].update([tag_2])
+
+    return {tag_1: list(app.tag_synonyms[tag_1])}
+
+
+@app.get("/get_tag_list/")
+async def get_tag_list():
+    return {"tag_list": app.names_storage}
+
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- ui.py
+++ ui.py
@@ -1,9 +1,12 @@
 import json
 import os
+from typing import List
 
 import gradio as gr
 import pandas as pd
 import requests
+
+from create_dummy_tags import tags
 
 # Define the URL for the FastAPI service
 BASE_URL = os.environ.get("API_BASE_URL", default="http://app:8000")
@@ -11,13 +14,13 @@ BASE_URL = os.environ.get("API_BASE_URL", default="http://app:8000")
 
 def upload_csv(file):
     filename = file.name
-    with open(filename, 'rb') as f:
-        files = {'file': (filename, f, 'text/csv')}
+    with open(filename, "rb") as f:
+        files = {"file": (filename, f, "text/csv")}
         response = requests.post(f"{BASE_URL}/upload-csv/", files=files)
         if response.status_code == 200:
-            return response.json()['message']
+            return response.json()["message"]
         else:
-            return response.json()['detail']
+            return response.json()["detail"]
 
 
 def search_names(query, k=5):
@@ -27,26 +30,26 @@ def search_names(query, k=5):
     if k < 1:
         raise gr.Error("Number of results should be at least 1")
 
-    response = requests.get(f"{BASE_URL}/search/", params={'query': query, 'k': k})
+    response = requests.get(f"{BASE_URL}/search/", params={"query": query, "k": k})
     if response.status_code == 200:
         data = response.json()
-        semantic_results = data['match']['semantic']
-        typo_results = data['match']['typo']
+        semantic_results = data["match"]["semantic"]
+        typo_results = data["match"]["typo"]
 
         semantic_df = pd.DataFrame(semantic_results)
         typo_df = pd.DataFrame(typo_results)
 
         return semantic_df, typo_df
     else:
-        return response.json()['detail'], pd.DataFrame()
+        return response.json()["detail"], pd.DataFrame()
 
 
 def delete_collection():
     response = requests.delete(f"{BASE_URL}/purge/")
     if response.status_code == 200:
-        return response.json()['message']
+        return response.json()["message"]
     else:
-        return response.json()['detail']
+        return response.json()["detail"]
 
 
 def check_task_status():
@@ -58,42 +61,115 @@ def check_task_status():
         return "Something bad happened"
 
 
+def add_synonym(tag_1: str, tag_2: str):
+    print("tag1:", tag_1)
+    print("tag2:", tag_2)
+    if not tag_1 or not tag_2:
+        raise gr.Error("Please select synonym tags")
+
+    response = requests.post(
+        f"{BASE_URL}/add_synonym/", params={"tag_1": tag_1, "tag_2": tag_2}
+    )
+    if response.status_code == 200:
+        return str(response.content)
+    else:
+        return "Something bad happened"
+
+
+def update_tag_list_dropdown():
+    response = requests.get(f"{BASE_URL}/get_tag_list/")
+    if response.status_code == 200:
+        return response.json()["tag_list"]
+    else:
+        gr.Error("someting bad happened")
+
+
 # Gradio interface
-with gr.Blocks(css="body { font-family: Arial, sans-serif; } footer { visibility: hidden }") as demo:
+with gr.Blocks(
+    css="body { font-family: Arial, sans-serif; } footer { visibility: hidden }"
+) as demo:
     gr.Markdown(
-        "<h1 style='text-align: center;'>Tag Search API</h1><h3 style='text-align: center;'>by the AI Team</h3>")
+        "<h1 style='text-align: center;'>Tag Search API</h1><h3 style='text-align: center;'>by the AI Team</h3>"
+    )
 
     with gr.Tab("Search"):
         with gr.Row():
-            query = gr.Textbox(label="Search Query", placeholder="Enter tag to search", elem_classes="tab-container")
-            k = gr.Number(label="Number of Results", value=5, elem_classes="tab-container")
+            query = gr.Textbox(
+                label="Search Query",
+                placeholder="Enter tag to search",
+                elem_classes="tab-container",
+            )
+            k = gr.Number(
+                label="Number of Results", value=5, elem_classes="tab-container"
+            )
         search_button = gr.Button("Search", elem_classes="tab-container")
-        semantic_table = gr.Dataframe(label="Semantic Matches", interactive=False, elem_classes="tab-container")
-        typo_table = gr.Dataframe(label="Typo Matches", interactive=False, elem_classes="tab-container")
+        semantic_table = gr.Dataframe(
+            label="Semantic Matches", interactive=False, elem_classes="tab-container"
+        )
+        typo_table = gr.Dataframe(
+            label="Typo Matches", interactive=False, elem_classes="tab-container"
+        )
 
-        search_button.click(fn=search_names, inputs=[query, k], outputs=[semantic_table, typo_table])
-        query.submit(fn=search_names, inputs=[query, k], outputs=[semantic_table, typo_table])
+        search_button.click(
+            fn=search_names, inputs=[query, k], outputs=[semantic_table, typo_table]
+        )
+        query.submit(
+            fn=search_names, inputs=[query, k], outputs=[semantic_table, typo_table]
+        )
 
     with gr.Tab("Upload CSV"):
         with gr.Accordion("Information", open=False):
-            gr.Markdown("""## Info
+            gr.Markdown(
+                """## Info
                         - the csv file must be **comma separated**
                         - the csv file **should have a 'name' column**, anything else will be discarded
                         - processing time depends on the number of records, you can check the status with the button below
-                        """)
-        csv_file = gr.File(label="Upload CSV File", type='filepath', elem_classes="tab-container")
+                        """
+            )
+        csv_file = gr.File(
+            label="Upload CSV File", type="filepath", elem_classes="tab-container"
+        )
         upload_button = gr.Button("Upload", elem_classes="tab-container")
-        upload_status = gr.Textbox(label="Upload Status", interactive=False, elem_classes="tab-container")
+        upload_status = gr.Textbox(
+            label="Upload Status", interactive=False, elem_classes="tab-container"
+        )
 
         upload_button.click(fn=upload_csv, inputs=csv_file, outputs=upload_status)
 
-        status_button_upload = gr.Button("Check Task Status", elem_classes="tab-container")
-        status_display_upload = gr.Textbox(label="Task Status", interactive=False, elem_classes="tab-container")
+        status_button_upload = gr.Button(
+            "Check Task Status", elem_classes="tab-container"
+        )
+        status_display_upload = gr.Textbox(
+            label="Task Status", interactive=False, elem_classes="tab-container"
+        )
         status_button_upload.click(fn=check_task_status, outputs=status_display_upload)
 
         delete_button = gr.Button("Delete Collection", elem_classes="tab-container")
-        delete_status = gr.Textbox(label="Delete Status", interactive=False, elem_classes="tab-container")
+        delete_status = gr.Textbox(
+            label="Delete Status", interactive=False, elem_classes="tab-container"
+        )
         delete_button.click(fn=delete_collection, outputs=delete_status)
+
+    with gr.Tab("Add Synonym"):
+
+        #!TODO load tag list dynamically from backend - not proud of this solution, but no idea why app.name_storage doesn't get filled with values on backend
+        list_dropdown_1 = gr.Dropdown(elem_classes="tab-container", choices=tags)
+        list_dropdown_2 = gr.Dropdown(elem_classes="tab-container", choices=tags)
+        # list_dropdown_1.select(fn=update_tag_list_dropdown,outputs=[list_dropdown_1])
+        # list_dropdown_2.select(fn=update_tag_list_dropdown,outputs=[list_dropdown_2])
+
+        update_button = gr.Button("Update tag list", elem_classes="tab-container")
+        update_status = gr.Textbox(
+            label="synonym update status",
+            interactive=False,
+            elem_classes="tab-container",
+        )
+        update_button.click(
+            fn=add_synonym,
+            inputs=[list_dropdown_1, list_dropdown_2],
+            outputs=update_status,
+        )
+
 
 # Launch the interface
 demo.launch(server_name="0.0.0.0")
